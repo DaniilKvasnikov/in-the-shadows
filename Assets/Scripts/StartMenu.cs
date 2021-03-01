2c9db9ebd55e1a20b3526db0eeb250b7301c356c
Даниил <6515122-GamerD_D@users.noreply.gitlab.com>
2021-03-01 22:38:17 +0300

    main menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] private Button normalModeButton;
+    [SerializeField] private Button testModeButton;
+    [SerializeField] private int nextSceneNum = 1;
+
+    private void Awake()
+    {
+        normalModeButton.onClick.AddListener(OpenLvl);
+        testModeButton.onClick.AddListener(OpenLvl);
+    }
+
+    private void OnDestroy()
+    {
+        normalModeButton.onClick.RemoveListener(OpenLvl);
+        testModeButton.onClick.RemoveListener(OpenLvl);
+    }
+
+    private void OpenLvl()
+    {
+        SceneManager.LoadScene(nextSceneNum);
+    }
+}
